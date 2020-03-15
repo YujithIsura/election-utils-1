84f3aee98be511adaecbff89d7c36d6932f3a323
Shalitha Vikum <hsvikum@gmail.com>
2020-03-15 18:37:38 +0530

    Added a service provider to communicate with the Nomination API with Auth headers and added a demonstration code to View PE9.Added a ENV variable to store the NOMINATION_API_KEY

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -3,19 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use GuzzleHttp\Client;
 
 class FormController extends Controller
 {
     public function viewPE9()
     {
-        $client = new Client();
-    	$response = $client->request('GET', env('NOMINETION_API'));
-    	$statusCode = $response->getStatusCode();
-        // $body = $response->getBody();
+        $client = resolve('nomination.client');
+        $response = $client->request('GET', 'teams');
+        $statusCode = $response->getStatusCode();
         $body = $response->getBody()->getContents();
         $obj = json_decode($body);
-        //dd($body);
         return view('pe9')->with('Fdata',$obj);
         
     }
