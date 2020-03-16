33dd220b0bd4ab1e08fde6d0d9620cc8dac49f33
Shanuka Alahakoon <52670260+ShanukaGIT@users.noreply.github.com>
2020-03-17 01:10:47 +0530

    Drop Down Fill Completed

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('index');
@@ -18,38 +8,14 @@ Route::get('index', function () {
     return view('index');
 });
 
-// Route::get('pe9', function () {
-
-//     return view('pe9');
-// });
-
-
-Route::get('pe10', function () {
-    return view('pe10');
-});
-// Route::get('pe30x1x2', function () {
-//     return view('pe30x1x2');
-// });
-
-Route::get('pe11', function () {
-    return view('pe11');
-});
-Route::get('pe10x1', function () {
-    return view('pe10x1');
-});
-
-
-Route::get('layout', function () {
-    return view('layout');
-});
-
-Route::get('pe9a', function () {
-    return view('pe9a');
-});
 
 // ----------------------------------------------------
 Route::get('/pe9', 'FormController@viewPE9');
+Route::get('/pe10', 'FormController@viewPE10');
+Route::get('/pe11', 'FormController@viewPE11');
+Route::get('/pe10x1', 'FormController@viewPE10X1');
 Route::get('/pe30x1x2', 'FormController@viewPE30');
+
 Route::get('/getDistrict', 'FormController@getDistrict');
 Route::get('/getBallot', 'FormController@getBallot');
 
--- storage/framework/views/2616f5c7aa32aa087e7558b363eed9b276f1f693.php
+++ storage/framework/views/2616f5c7aa32aa087e7558b363eed9b276f1f693.php
@@ -1,97 +1,100 @@
 <?php $__env->startSection('content'); ?>
 
-<?php 
-$selec=0;
- ?>
 
-<div class="container">
+
   <div class="row justify-content-md-center">
-    <div class="col col-lg-3">
-  <form> <center>
-<select class="custom-select custom-select-lg mb-3" class="election" name="election"onChange >
-<option value="0" disabled="true" selected="true">Select Election</option> 
-    <?php $__currentLoopData = $Fdata; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $data): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-      <option value="<?php echo e($data->id); ?>"><?php echo e($data->name); ?></option>
-    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
-</select>
-</div>
-<div class="col col-lg-3">
+      <form> 
+        <div class="row">
+          <div class="col col-lg-5">
+            <select class="custom-select custom-select-md mb-3" class="election" name="election">
+                <option value="0" disabled="true" selected="true">Select Election</option> 
+                <?php $__currentLoopData = $Fdata; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $data): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                  <option value="<?php echo e($data->id); ?>"><?php echo e($data->name); ?></option>
+                <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+            </select>
+          </div>
+          <div class="col col-lg-5">
+              <select class="custom-select custom-select-md mb-3" class="district" name="district" >
+                  <option value="0" disabled="true" selected="true">Select District</option> 
+              </select>
+          </div>
 
-<select class="custom-select custom-select-lg mb-3" class="district" name="district" >
-    <option value="0" disabled="true" selected="true">Select District</option> 
-</select>
-
-</div>
-</div>  </div> 
-<center>
-<Button type="button" class="btn btn-primary" id="bellot">Generate Report </Button>
-</center>
-</form>
-  </center>
- </div>
-  <section class="page-section clearfix">
-    <div class="container" id="example">
-      
-       
-        <div class="intro-text left-0 text-center bg-faded p-5 rounded">
-        
-          <h6 class="section-heading mb-10" align="left">
-            <span>30(1)(2) ආ වගන්තිය <br>
-            30(1)(2) ஆம் பிரிவு <br>
-            Section Under 30(1)(2) </span>
-            
-          </h6>
-         
-          <h2 class="section-heading mb-4">
-            <span>PARLIAMENTARY ELECTIONS 2020 <br>පාර්ලිමේන්තු මැතිවරණය 2020</span>
-            
-          </h2>
-          <h5 class="section-heading mb-4">
-            <span>
-            බැලට් පේපර්<br> பாலோட் பேப்பர்<br>Ballot Paper<br>
-             </span>
-            
-          </h5>
-         <p> Ballot Paper No:...............................................................................................................................................................................</p>
-         <table align="center" border="1" cellpadding="5" width="80%" id="tblBellot">
-         <thead>
-         	<tr>
-             <th align="right"> Party Name</th>
-             <th align="left"> Symbol</th>
-             <th align="left"> No</th>
-          </tr>
-          </thead>
-          <tbody>
-          
-         </tbody>
-            
-             </table >
-             <table align="center" border="1" cellpadding="5" width="80%">
-             <tr>
-             <td>1</td><td>2</td><td>3</td><td>4</td><td>5</td><td>6</td><td>7</td><td>8</td><td>9</td><td>10</td>
-             </tr>
-             <tr>
-             <td>11</td><td>12</td><td>13</td><td>14</td><td>15</td><td>16</td><td>17</td><td>18</td><td>19</td><td>20</td>
-             </tr>
-             <tr>
-             <td>21</td><td>22</td>
-             </tr>
-             </table>
-         <br>
-        
-        <br><br>
-        
-            
-            
+           <div class="col col-md-2">
+              <Button type="button" class="btn btn-primary" id="bellot">Generate</Button>
           </div>
 
         </div>
+           
+      </form>
+  </div>  
 
+  <section class="page-section clearfix mt-2">
+    <div class="container" id="example">
       
-    </div><br> 
-    <center>  <input class="btn btn-primary" type="button" onclick="printDiv('example')" value="Print Data" /> </center>
-  </section>
+        <div class="intro-text left-0 text-center bg-faded p-5 rounded">
+        
+            <h6 class="section-heading mb-10" align="left">
+              <span>30(1)(2) ආ වගන්තිය <br>
+              30(1)(2) ஆம் பிரிவு <br>
+              Section Under 30(1)(2) </span>
+            </h6>
+           
+            <h2 class="section-heading mb-4">
+              <span>PARLIAMENTARY ELECTIONS 2020 <br>පාර්ලිමේන්තු මැතිවරණය 2020</span>  
+            </h2>
 
+            <h5 class="section-heading mb-4">
+              <span>
+              බැලට් පේපර්<br> பாலோட் பேப்பர்<br>Ballot Paper<br>
+              </span> 
+            </h5>
+
+           <p> Ballot Paper No:...............................................................................................................................................................................
+           </p>
+
+           <table align="center" border="1" cellpadding="5" width="80%" id="tblBellot">
+           <thead>
+             	<tr>
+                 <th align="right"> Party Name</th>
+                 <th align="left"> Symbol</th>
+                 <th align="left"> No</th>
+              </tr>
+            </thead>
+            <tbody>
+
+            
+           </tbody>  
+          </table >
+
+         <table align="center" border="1" cellpadding="5" width="80%">
+             <tr>
+                <td>1</td><td>2</td><td>3</td><td>4</td><td>5</td><td>6</td><td>7</td><td>8</td><td>9</td><td>10</td>
+             </tr>
+             <tr>
+                <td>11</td><td>12</td><td>13</td><td>14</td><td>15</td><td>16</td><td>17</td><td>18</td><td>19</td><td>20</td>
+             </tr>
+             <tr>
+                <td>21</td><td>22</td>
+             </tr>
+         </table>
+           
+      </div>
+
+    </div>
+
+  <div class="container">
+  <center> 
+      <br> 
+      <input class="btn btn-primary" type="button" onclick="printDiv('example')" value="Print Data" /> 
+  </center>
+</div>
+</section> 
+
+
+
+
+    
+  
   <?php $__env->startPush('scripts'); ?>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
   <script>
@@ -100,7 +103,7 @@ $selec=0;
 
 // -------------------------Get District----------------------------------
   $("select[name='election']").change(function () {
-
+      $("#tblBellot tbody").empty();
       var elec_id=$(this).val();
       var op=" ";
 
@@ -127,34 +130,40 @@ $selec=0;
 
 // -------------------------Get Bellot----------------------------------
 $("#bellot").click(function () {
-  var elec_id = $("select[name='election']").val();
   var dist_id = $("select[name='district']").val();
   var op=" ";
+  // alert(dist_id);
 
 $.ajax({
   type:'GET',
   url:'<?php echo URL::to('getBallot'); ?>',
-  data:{'elecid':elec_id,'disid':dist_id},
+  data:{'id':dist_id},
+  
   success:function(data){
 
     for(var i=0;i<data.length;i++){
 
       op+='<tr>';
-      op+='<td>'+data[i].id+'</td><td>'+data[i].name+'</td><td>'+data[i].username+'</td>';
-      op+='</tr>';
-    
+      op+='<td>'+data.parties[i].id+'</td><td>'+data.parties[i].name+'</td><td>'+ddata.parties[i].symbol+'</td>';
+      op+='</tr>';   
     }
+    
     $("#tblBellot tbody").empty();
     $("#tblBellot tbody").append(op);
 
   },
   error:function(){
+    alert("error");
 
   }
 });
 });
 
 // ---------------------------------------------------------------------
+$("select[name='district']").change(function () {
+      $("#tblBellot tbody").empty();
+});
+
 
 
 // });
